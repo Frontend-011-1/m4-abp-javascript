4ce0325a9d020089bb58d4bdebda38e20a7ab86c
iVerdugoDev <ivan270@gmail.com>
2026-01-28 12:57:05 -0300

    agrega ejercicio 2 de la presentacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // Lección 1:
 console.log('Hola desde JS');
 // Prompt permite pedirle al usuario un dato o mensaje, a través de un mensaje en pantalla
-prompt('Escriba un número');
+// prompt('Escriba un número');
 // Alert: sirve para mostrar un mensaje informativo. OJO! Detiene la ejecución del programa
-alert('PÁRELE!');
+// alert('PÁRELE!');
 
-// let parrafo = document.getElementById()
+// Lección 1 - Ejercicio 2:
+let parrafo = document.getElementById('mensaje');
+parrafo.textContent = 'Contenido del párrafo cambiado con JS';
